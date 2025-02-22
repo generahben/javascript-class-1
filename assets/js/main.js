4d1c451634cb03ce050416a797c1cf406a0d7fdd
Generah Ben <generahben@gmail.com>
2025-02-22 06:08:05 -0600

    Add Internal and External JavaScript

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.write('This is written with Javascript!');
+    document.getElementById('title').innerText = "This Title Is Written From Javascript!";
+    document.getElementById('answer').innerText = 5 + 5;
+
+    console.log('Her name is Abigail!')
+    console.warn('Stop Eating in Class and Focus!')
+    console.error('May we not see error wey pass us Ooo!')
