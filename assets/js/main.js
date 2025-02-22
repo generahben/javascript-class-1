00a3d3870db7173881761abc12df1fe1a88eb295
Generah Ben <generahben@gmail.com>
2025-02-22 07:05:53 -0600

    Add Javascript Data Types and Variable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,49 @@
-document.write('This is written with Javascript!');
-    document.getElementById('title').innerText = "This Title Is Written From Javascript!";
-    document.getElementById('answer').innerText = 5 + 5;
+document.write('hello world and this is javascript')
 
-    console.log('Her name is Abigail!')
-    console.warn('Stop Eating in Class and Focus!')
-    console.error('May we not see error wey pass us Ooo!')
+document.getElementById('demo').innerText = "This is a TExt from Javascript";
+
+// const dateOfBirth = 'June 12';
+// let myName = "Mr Ben";
+
+// console.log(myName + ' was born on ' + dateOfBirth)
+
+// myName = "Mr Sam"
+// dateOfBirth = "March 16";
+
+// console.log(myName + ' was born on ' + dateOfBirth)
+
+
+const variableNameForThisClass = 'something';
+const anotherVariableForThisClass = ' another thing';
+
+const aStatement = variableNameForThisClass + anotherVariableForThisClass;
+
+console.log(aStatement)
+
+
+const y = 5;
+const x = 4;
+
+const answer = y + x;
+
+console.log(answer)
+
+let isMrBenMarried = true
+let isMrSamMarried = false
+
+console.log(isMrBenMarried, isMrSamMarried)
+
+
+const beforeThisWorldWasCreated = null;
+// let whoDestroyedThisWorld = undefined
+
+console.log(beforeThisWorldWasCreated, whoDestroyedThisWorld)
+
+
+console.log('The typeof variableNameForThisClass is: ' + typeof variableNameForThisClass)
+console.log('The typeof x and y are: ' + typeof x)
+console.log('The typeof isMrBenMarried and isMrSamMarried are: ' + typeof isMrSamMarried)
+
+
+
+
