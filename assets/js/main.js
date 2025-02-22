73a9c6edfb468e97d7df321222082e04fc105a54
Generah Ben <generahben@gmail.com>
2025-02-22 08:04:13 -0600

    Start Conditions in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,16 @@ document.getElementById('demo').innerText = "This is a TExt from Javascript";
 const variableNameForThisClass = 'something';
 const anotherVariableForThisClass = ' another thing';
 
+console.log(variableNameForThisClass.toUpperCase())
+
 const aStatement = variableNameForThisClass + anotherVariableForThisClass;
 
 console.log(aStatement)
 
 
 const y = 5;
-const x = 4;
+const x = 4; //number (4.666 (float))
+
 
 const answer = y + x;
 
@@ -35,14 +38,44 @@ console.log(isMrBenMarried, isMrSamMarried)
 
 
 const beforeThisWorldWasCreated = null;
-// let whoDestroyedThisWorld = undefined
+let whoDestroyedThisWorld
 
 console.log(beforeThisWorldWasCreated, whoDestroyedThisWorld)
 
 
+let person = {
+    name: "Esther",
+    gender: "Female",
+    age: 15
+}
+
+console.log(person.gender)
+
+
+let nameOfPeopleInGenerahSystemsToday = ['Ben', 'Sam', 'Esther', 'Abigail']
+
+
+console.log(nameOfPeopleInGenerahSystemsToday)
+
 console.log('The typeof variableNameForThisClass is: ' + typeof variableNameForThisClass)
 console.log('The typeof x and y are: ' + typeof x)
 console.log('The typeof isMrBenMarried and isMrSamMarried are: ' + typeof isMrSamMarried)
+console.log('The typeof person is: ' + typeof person)
+
+
+
+console.log("########### THIS SESSION FOR ARITHMETIC###############")
+
+console.log('Addition (x and y): ' + (y + x))
+console.log('Subtraction (x and y): ' + y - x)
+console.log('Division (x and y): ' + y / x)
+console.log('Multiplication (x and y): ' + y * x)
+console.log('Modulus (x and y): ' + y % x)
+
+let esther = 'female';
+let abigail = 'Female';
+
+console.log (esther == abigail);
 
 
 
